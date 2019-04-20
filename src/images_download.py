c0ad880dd6e2bda6d3774b2f19373c4188a7f10e
InspireX96 <xume1996@gmail.com>
2019-04-19 23:18:41 -0700

    cleanup code

--- src/images_download.py
+++ src/images_download.py
@@ -1,18 +1,18 @@
+import ssl
 import json
 import tqdm
+import time
 import urllib.request
 from multiprocessing import Pool
-import time
 
-
-import ssl
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
 training_raw = '../data/raw/imaterialist-product-2019/train.json'
 training_repo = '../data/training_set'
 
-class load_and_download:
+
+class Load_and_download:
     def __init__(self, json_str, repo):
         self.json_str = json_str
         # self.raw = raw
@@ -30,6 +30,10 @@ class load_and_download:
 
     @staticmethod
     def download(case_repo):
+        '''
+        download images
+        input: case_repo, zip of case(dict) and repo(list)
+        '''
         case, repo = case_repo[0], case_repo[1]
 
         try:
@@ -38,33 +42,27 @@ class load_and_download:
             print(' \n404: {}\n'.format(case))
 
 
-
 if __name__ == '__main__':
 
-    training = load_and_download(training_raw, training_repo)
+    training = Load_and_download(training_raw, training_repo)
     training_dict = training.load()
 
+    # perpare for download
     case_input = training_dict['images']
     length = len(case_input)
     repo_input = [training_repo for i in range(length)]
-    
+
+    # download using multiprocessing
     pool = Pool()
     print('downloading')
+    t_1 = time.clock()
     # pool.map(load_and_download.download, zip(case_input, repo_input))
     # pool.close()
     # pool.join()
-    for _ in tqdm.tqdm(pool.imap_unordered(training.download, zip(case_input,repo_input)), total=length):
+    for _ in tqdm.tqdm(pool.imap_unordered(training.download, zip(case_input, repo_input)), total=length):
         pass
 
     pool.close()
     pool.join()
-
-    # count = 0
-    # for case_repo in zip(case_input,repo_input):
-    #     training.download(case_repo)
-    #     count += 1
-    #     if count > 5:
-    #         break
-
-
-
+    print('elapsed time: ', time.clock() - t_1)
+    print('download complete')
