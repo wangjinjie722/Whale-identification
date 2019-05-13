4555d15dc3b8df957ebbbe5f677f862e9470049f
Rebachtr <ruoqizh@gmail.com>
2019-05-12 20:58:11 -0700

    Update res18mod.py

--- src/res18mod.py
+++ src/res18mod.py
@@ -39,7 +39,7 @@ class ResidualBlock(nn.Module):
         return out
 
 class ResNet(nn.Module):
-    def __init__(self, ResidualBlock, num_classes=10):
+    def __init__(self, ResidualBlock, num_classes=4252):
         super(ResNet, self).__init__()
         self.inchannel = 64
         self.conv1 = nn.Sequential(
