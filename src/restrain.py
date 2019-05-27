4dd015c6305e521ae8798cb3c2ce9e877a7f2a27
Rebachtr <ruoqizh@gmail.com>
2019-05-27 14:34:32 -0700

    1

--- src/restrain.py
+++ src/restrain.py
@@ -18,7 +18,7 @@ from res18mod import ResNet18
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # 参数设置,使得我们能够手动输入命令行参数，就是让风格变得和Linux命令行差不多
-parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
+parser = argparse.ArgumentParser(description='PyTorch Whale Training')
 parser.add_argument('--outf', default='./model/', help='folder to output images and model checkpoints') #输出结果保存路径
 parser.add_argument('--net', default='./model/Resnet18.pth', help="path to net (to continue training)")  #恢复训练时的模型路径
 args = parser.parse_args()
@@ -29,25 +29,77 @@ pre_epoch = 0  # 定义已经遍历数据集的次数
 BATCH_SIZE = 128      #批处理尺寸(batch_size)
 LR = 0.1        #学习率
 
+class WhaleDataset(Dataset):
+    def __init__(self, datafolder, datatype='train', df=None, transform = transforms.Compose([transforms.ToTensor()]), y=None
+                ):
+        self.datafolder = datafolder
+        self.datatype = datatype
+        self.y = y
+        if self.datatype == 'train':
+            self.df = df.values
+        self.image_files_list = [s for s in os.listdir(datafolder)]
+        self.transform = transform
+
+
+    def __len__(self):
+        return len(self.image_files_list)
+    
+    def __getitem__(self, idx):
+        if self.datatype == 'train':
+            img_name = os.path.join(self.datafolder, self.df[idx][0])
+            label = self.y[idx]
+            
+        elif self.datatype == 'test':
+            img_name = os.path.join(self.datafolder, self.image_files_list[idx])
+            label = np.zeros((5005,))
+
+        img = cv2.imread(img_name)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        image = self.transform(image=img)
+        image = image['image']
+        if self.datatype == 'train':
+            return image, label
+        elif self.datatype == 'test':
+            # so that the images will be in a correct order
+            return image, label, self.image_files_list[idx]
+
+data_transforms = transforms.Compose([
+                                      transforms.Resize((100, 100)),
+                                      transforms.ToTensor(),
+                                      transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                                             std=[0.229, 0.224, 0.225])
+    ])
+data_transforms_test = transforms.Compose([
+                                           transforms.Resize((100, 100)),
+                                           transforms.ToTensor(),
+                                           transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                                                 std=[0.229, 0.224, 0.225])
+])        
+
+train_dataset = WhaleDataset(datafolder='./whaletrain/', datatype='train', df=train_df, transform=data_transforms, y=y)
+train_sampler = SubsetRandomSampler(list(range(len(os.listdir('./whaletrain/')))))
+batch_size = 512
+num_workers = 2
+train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, sampler=train_sampler, num_workers=num_workers)
 # 准备数据集并预处理
-transform_train = transforms.Compose([
-    transforms.RandomCrop(32, padding=4),  #先四周填充0，在吧图像随机裁剪成32*32
-    transforms.RandomHorizontalFlip(),  #图像一半的概率翻转，一半的概率不翻转
-    transforms.ToTensor(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)), #R,G,B每层的归一化用到的均值和方差
-])
+#transform_train = transforms.Compose([
+#    transforms.RandomCrop(32, padding=4),  #先四周填充0，在吧图像随机裁剪成32*32
+#    transforms.RandomHorizontalFlip(),  #图像一半的概率翻转，一半的概率不翻转
+#    transforms.ToTensor(),
+#    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)), #R,G,B每层的归一化用到的均值和方差
+#])
 
-transform_test = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-])
-trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train) #训练数据集
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=BATCH_SIZE, shuffle=True, num_workers=2)   #生成一个个batch进行批训练，组成batch的时候顺序打乱取
+#transform_test = transforms.Compose([
+#    transforms.ToTensor(),
+#    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+#])
+#trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train) #训练数据集
+#trainloader = torch.utils.data.DataLoader(trainset, batch_size=BATCH_SIZE, shuffle=True, num_workers=2)   #生成一个个batch进行批训练，组成batch的时候顺序打乱取
 
-testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_test)
-testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=2)
+#testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_test)
+#testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=2)
 # Cifar-10的标签
-classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+#classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
 # 模型定义-ResNet
 net = ResNet18().to(device)
